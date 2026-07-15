7b49bd1bcf6901f40c42cbc22983096e0cd846ce
itc-n26020 <n26020@std.it-college.ac.jp>
2026-07-15 03:00:07 +0000

    4.3

---
+++ chap04/q4_3_1.py
@@ -0,0 +1,7 @@
+names = ('a', 'b', 'c')
+sep = '_'
+sep.join(names)
+def concat_words(*args, separator='.'):
+    return separator.join(args)
+print(concat_words('a', 'b', 'c', 'd', separator='_'))
+print(concat_words('4_choume', 'Minatoku', 'Tokyo', 'Japan', separator='_'))
