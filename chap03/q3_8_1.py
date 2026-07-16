3ba83b1d78f606a1197e4608481585811e707995
itc-n26020 <n26020@std.it-college.ac.jp>
2026-07-16 03:27:55 +0000

    3.8

--- chap03/q3_8_1.py
+++ chap03/q3_8_1.py
@@ -1,5 +1,4 @@
 import pickle
-
 with open('test1.pk1', 'wb') as f:
     my_list = list(range(1,11))
     pickle.dump(my_list, f)
--- chap03/q3_8_2.py
+++ chap03/q3_8_2.py
@@ -1,4 +1,4 @@
 import pickle
-with open('test1.pkl', 'rb') as f:
+with open('test1.pk1', 'rb') as f:
     result = pickle.load(f)
     print(result)
